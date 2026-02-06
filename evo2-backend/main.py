69b60ca4f26ca352cdfe724a24935ac9eea605f2
Anaghh <anaghcode@gmail.com>
2026-02-06 18:22:03 +0530

    Update main.py

--- evo2-backend/main.py
+++ evo2-backend/main.py
@@ -12,19 +12,12 @@ class VariantRequest(BaseModel):
 
 evo2_image = (
     modal.Image.from_registry(
-        "nvidia/cuda:12.4.0-devel-ubuntu22.04", add_python="3.12"
+        "nvcr.io/nvidia/pytorch:25.01-py3"
     )
-    .apt_install(
-        ["build-essential", "cmake", "ninja-build",
-            "libcudnn8", "libcudnn8-dev", "git", "gcc", "g++"]
-    )
-    .env({
-        "CC": "/usr/bin/gcc",
-        "CXX": "/usr/bin/g++",
-    })
+    .apt_install(["git"])
+    .run_commands("pip install --upgrade pip")
     .run_commands("git clone --recurse-submodules https://github.com/ArcInstitute/evo2.git && cd evo2 && pip install .")
-    .run_commands("pip uninstall -y transformer-engine transformer_engine")
-    .run_commands("pip install 'transformer_engine[pytorch]==1.13' --no-build-isolation")
+    .run_commands("pip install biopython huggingface_hub")
     .pip_install_from_requirements("requirements.txt")
 )
 
